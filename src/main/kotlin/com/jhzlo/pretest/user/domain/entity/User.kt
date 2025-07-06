3bd35944191d6cb54fb7b4c1fc05ee419e252561
Jhzlo <kjh0010703@naver.com>
2025-07-06 23:48:10 +0900

    fix: postgres user 예약어 변경 -> users

--- src/main/kotlin/com/jhzlo/pretest/user/domain/entity/User.kt
+++ src/main/kotlin/com/jhzlo/pretest/user/domain/entity/User.kt
@@ -13,7 +13,7 @@ import jakarta.persistence.Id
 import jakarta.persistence.Table
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
